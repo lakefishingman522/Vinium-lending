e20a2d9edc04b465074038885eccca3fb43c3dfb
lakefishingman522 <calvingill89@gmail.com>
2022-10-04 01:10:02 +0900

    add getSourceOfAsset function in interface

--- contracts/misc/interfaces/IAaveOracle.sol
+++ contracts/misc/interfaces/IAaveOracle.sol
@@ -8,10 +8,18 @@ pragma solidity 0.6.12;
 
 interface IAaveOracle {
   function BASE_CURRENCY() external view returns (address); // if usd returns 0x0, if eth returns weth address
+
   function BASE_CURRENCY_UNIT() external view returns (uint256);
 
   /***********
     @dev returns the asset price in ETH
      */
   function getAssetPrice(address asset) external view returns (uint256);
+
+  /**
+   * @notice Returns the address of the source for an asset address
+   * @param asset The address of the asset
+   * @return The address of the source
+   */
+  function getSourceOfAsset(address asset) external view returns (address);
 }
